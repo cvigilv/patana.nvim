fb9e86824deda728aff78b023833ff27c77139b3
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-06-10 00:23:17 -0400

    revert: embrace default treesitter colorgroups and add some basic ones

--- colors/patana.lua
+++ colors/patana.lua
@@ -126,7 +126,7 @@ local hlgroups = {
 	Title = { link = "Constant" },
 	--}}}
 	-- syntax {{{
-	Function = { fg = palette.norm },
+	Function = { fg = palette.norm, bold = true },
 	Identifier = { link = "Function" },
 
 	Statement = { bold = true },
@@ -147,7 +147,7 @@ local hlgroups = {
 	Structure = { link = "Type" },
 	Typedef = { link = "Type" },
 
-	Operator = { fg = palette.accent, bold = true },
+	Operator = { fg = palette.accent },
 	Debug = { link = "Operator" },
 
 	Special = { italic = true },
@@ -274,115 +274,7 @@ local hlgroups = {
 	GitDelete = { link = "Removed" },
 	--}}}
 	-- treesitter {{{
-	["@variable"] = { link = "Identifier" },
-	["@variable.builtin"] = { link = "Type" },
-	["@variable.parameter"] = { link = "Identifier" },
-	["@variable.member"] = { link = "Identifier" },
-
-	["@constant"] = { link = "Constant" },
-	["@constant.builtin"] = { link = "Type" },
-	["@constant.macro"] = { link = "Define" },
-
-	["@module"] = { link = "Include" },
-	["@module.builtin"] = { link = "Include" },
-	["@label"] = { link = "Label" },
-
-	["@string"] = { link = "String" },
-	["@string.documentation"] = { link = "String" },
-	["@string.regexp"] = { link = "String" },
-	["@string.escape"] = { link = "SpecialChar" },
-	["@string.special"] = { link = "SpecialChar" },
-	["@string.special.symbol"] = { link = "Identifier" },
-	["@string.special.path"] = { link = "Underlined" },
-	["@string.special.url"] = { link = "Underlined" },
-
-	["@character"] = { link = "Character" },
-	["@character.special"] = { link = "SpecialChar" },
-
-	["@boolean"] = { link = "Boolean" },
-	["@number"] = { link = "Number" },
-	["@number.float"] = { link = "Float" },
-
-	["@type"] = { link = "Type" },
-	["@type.builtin"] = { link = "Type" },
-	["@type.qualifier"] = { link = "Type" },
-	["@type.definition"] = { link = "Typedef" },
-
-	["@attribute"] = { link = "PreProc" },
-	["@property"] = { link = "Identifier" },
-
-	["@function"] = { link = "Function" },
-	["@function.builtin"] = { link = "Special" },
-	["@function.call"] = { link = "Special" },
-	["@function.macro"] = { link = "Macro" },
-
-	["@function.method"] = { link = "@function" },
-	["@function.method.call"] = { link = "@function.call" },
-
-	["@constructor"] = { link = "Identifier" },
-	["@operator"] = { link = "Operator" },
-
-	["@keyword"] = { link = "Keyword" },
-	["@keyword.coroutine"] = { link = "Keyword" },
-	["@keyword.function"] = { link = "Function" },
-	["@keyword.operator"] = { link = "Operator" },
-	["@keyword.import"] = { link = "Include" },
-	["@keyword.storage"] = { link = "Keyword" },
-	["@keyword.repeat"] = { link = "Repeat" },
-	["@keyword.return"] = { link = "Keyword" },
-	["@keyword.debug"] = { link = "Debug" },
-	["@keyword.exception"] = { link = "Exception" },
-
-	["@keyword.conditional"] = { link = "Conditional" },
-	["@keyword.conditional.ternary"] = { link = "Conditional" },
-	["@keyword.directive"] = { link = "PreProc" },
-	["@keyword.directive.define"] = { link = "PreProc" },
-
-	["@punctutation.delimiter"] = { link = "Delimiter" },
-	["@punctutation.bracket"] = { link = "Delimiter" },
-	["@punctutation.special"] = { link = "Delimiter" },
-
-	["@comment"] = { link = "Comment" },
-	["@comment.documentation"] = { link = "Comment" },
-
-	["@comment.warning"] = { link = "WarningMsg" },
-	["@comment.error"] = { link = "Error" },
-	["@comment.todo"] = { link = "Todo" },
-	["@comment.note"] = { link = "SpecialComment" },
-
-	["@markup.strong"] = { bold = true },
-	["@markup.emphasis"] = { italic = true },
-	["@markup.underline"] = { underline = true },
-	["@markup.strike"] = { strikethrough = true },
-
-	["@markup.heading"] = { link = "Title" },
-	["@markup.heading.gitcommit"] = { link = "@spell" },
-
-	["@markup.quote"] = { link = "Comment" },
-	["@markup.math"] = { link = "Special" },
-	["@markup.environment"] = { link = "Macro" },
-
-	["@markup.link"] = { link = "Underlined" },
-	["@markup.link.label"] = { link = "SpecialChar" },
-	["@markup.link.url"] = { link = "Keyword" },
-
-	["@markup.raw"] = { link = "SpecialComment" },
-	["@markup.raw.block"] = {},
-
-	["@markup.list"] = { link = "Identifier" },
-	["@markup.list.checked"] = { link = "Comment" },
-	["@markup.list.unchecked"] = { link = "Identifier" },
-
-	["@tag"] = { link = "Label" },
-	["@tag.delimiter"] = { link = "Delimiter" },
-	["@tag.attribute"] = { link = "Identifier" },
-
-	["@diff.plus"] = { link = "DiffAdd" },
-	["@diff.minus"] = { link = "DiffDelete" },
-	["@diff.delta"] = { link = "DiffChange" },
-
-	["@string.documentation.python"] = { link = "Comment" },
-
+	["@string.documentation"] = { link = "Comment" },
 	["@keyword.function.julia"] = { bold = true },
 	--}}}
 	-- quickscope.vim {{{
