dcef8757466f6597272971c1348238c5bbe05f6e
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-06-13 00:42:20 -0400

    refactor: add color hues and improve diff colors

--- colors/patana.lua
+++ colors/patana.lua
@@ -14,24 +14,59 @@ local colors = {
 	grays = {
 		["000"] = "#000000",
 		["050"] = "#0E0E0E",
-		["100"] = "#1C1C1C",
+		["100"] = "#1C1C1C", -- darker
 		["150"] = "#2A2A29",
 		["200"] = "#393837",
 		["250"] = "#474645",
-		["300"] = "#555453",
+		["300"] = "#555453", -- dark
 		["350"] = "#636261",
 		["400"] = "#71706F",
 		["450"] = "#807F7D",
-		["500"] = "#8E8D8A",
+		["500"] = "#8E8D8A", -- normal
 		["550"] = "#9C9B98",
 		["600"] = "#AAA9A6",
 		["650"] = "#B8B7B4",
-		["700"] = "#C6C5C2",
+		["700"] = "#C6C5C2", -- light
 		["750"] = "#D5D3D0",
 		["800"] = "#E3E1DD",
 		["850"] = "#F1EFEB",
-		["900"] = "#FFFDF9",
+		["900"] = "#FFFDF9", -- lighter
 	},
+	greens = {
+		["100"] = "#182612", -- darker
+		["200"] = "#304d23",
+		["300"] = "#487335", -- dark
+		["400"] = "#609a47",
+		["500"] = "#7bb662", -- normal
+		["600"] = "#96c581",
+		["700"] = "#b0d4a1", -- light
+		["800"] = "#cae2c0",
+		["900"] = "#e5f1e0", -- lighter
+	},
+	oranges = {
+		["100"] = "#361903", -- darker
+		["200"] = "#6b3105",
+		["300"] = "#a14a08", -- dark
+		["400"] = "#d6630a",
+		["500"] = "#f58024", -- normal
+		["600"] = "#f79850",
+		["700"] = "#f9b27b", -- light
+		["800"] = "#fbcca7",
+		["900"] = "#fde5d3", -- lighter
+	},
+
+	purples = {
+		["100"] = "#1e1023", -- darker
+		["200"] = "#3d1f47",
+		["300"] = "#5b2f6a", -- dark
+		["400"] = "#7a3e8e",
+		["500"] = "#994fb2", -- normal
+		["600"] = "#ad71c1",
+		["700"] = "#c195d0", -- light
+		["800"] = "#d6b8e0",
+		["900"] = "#eadcef", -- lighter
+	},
+
 	green_pastel = "#B3D8AB",
 	green_light = "#7BB662",
 	green_dark = "#589C48",
@@ -258,10 +293,10 @@ local hlgroups = {
 	DiagnosticSignWarn = { link = "DiagnosticWarn" },
 	--}}}
 	-- git-related {{{
-	Added = { fg = palette.fg, bg = palette.add },
-	Changed = { fg = palette.fg, bg = palette.change },
-	Removed = { fg = palette.fg, bg = palette.delete },
-	Deleted = { fg = palette.fg, bg = palette.delete },
+	Added = { fg = colors.greens["100"], bg = colors.greens["900"], reverse = vim.o.background == "dark" },
+	Changed = { fg = colors.oranges["100"], bg = colors.oranges["900"], reverse = vim.o.background == "dark" },
+	Removed = { fg = colors.purples["100"], bg = colors.purples["900"], reverse = vim.o.background == "dark" },
+	Deleted = { fg = colors.purples["100"], bg = colors.purples["900"], reverse = vim.o.background == "dark" },
 
 	DiffAdd = { link = "Added" },
 	DiffChange = { link = "Changed" },
