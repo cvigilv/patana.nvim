4179edaf86a8c91e144d8ef0f5379ede281a3609
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-07-17 00:42:34 -0400

    refactor: split into 3 variables

--- colors/patana.lua
+++ colors/patana.lua
@@ -32,67 +32,9 @@ vim.g.patana_sidebar_filetypes = {
 	"diff",
 	"gitcommit",
 }
-if vim.g.patana_color_order == nil then
-	vim.g.patana_color_order = DEFAULT_ORDER
-end
-
-local colors = {
-	grays = {
-		["000"] = "#000000",
-		["050"] = "#0E0E0E",
-		["100"] = "#1C1C1C", -- darker
-		["150"] = "#2A2A29",
-		["200"] = "#393837",
-		["250"] = "#474645",
-		["300"] = "#555453", -- dark
-		["350"] = "#636261",
-		["400"] = "#71706F",
-		["450"] = "#807F7D",
-		["500"] = "#8E8D8A", -- normal
-		["550"] = "#9C9B98",
-		["600"] = "#AAA9A6",
-		["650"] = "#B8B7B4",
-		["700"] = "#C6C5C2", -- light
-		["750"] = "#D5D3D0",
-		["800"] = "#E3E1DD",
-		["850"] = "#F1EFEB",
-		["900"] = "#FFFDF9", -- lighter
-	},
-	greens = {
-		["100"] = "#182612", -- darker
-		["200"] = "#304d23",
-		["300"] = "#487335", -- dark
-		["400"] = "#609a47",
-		["500"] = "#7bb662", -- normal
-		["600"] = "#96c581",
-		["700"] = "#b0d4a1", -- light
-		["800"] = "#cae2c0",
-		["900"] = "#e5f1e0", -- lighter
-	},
-	oranges = {
-		["100"] = "#361903", -- darker
-		["200"] = "#6b3105",
-		["300"] = "#a14a08", -- dark
-		["400"] = "#d6630a",
-		["500"] = "#f58024", -- normal
-		["600"] = "#f79850",
-		["700"] = "#f9b27b", -- light
-		["800"] = "#fbcca7",
-		["900"] = "#fde5d3", -- lighter
-	},
-
-	purples = {
-		["100"] = "#1e1023", -- darker
-		["200"] = "#3d1f47",
-		["300"] = "#5b2f6a", -- dark
-		["400"] = "#7a3e8e",
-		["500"] = "#994fb2", -- normal
-		["600"] = "#ad71c1",
-		["700"] = "#c195d0", -- light
-		["800"] = "#d6b8e0",
-		["900"] = "#eadcef", -- lighter
-	},
-}
+vim.g.patana_primary_color = vim.g.patana_primary_color or "greens"
+vim.g.patana_secondary_color = vim.g.patana_secondary_color or "oranges"
+vim.g.patana_accent_color = vim.g.patana_accent_color or "purples"
 
 local contrasting = {
 	greens = { fg = colors.grays["000"], bg = colors.greens["700"] },
@@ -144,9 +86,9 @@ if vim.o.background == "dark" then
 		norm_very_subtle = colors.grays["700"],
 
     -- Colors
-		primary          = colors[primary]["500"],
-		secondary        = colors[secondary]["500"],
-		accent           = colors[accent]["500"],
+		primary          = colors[vim.g.patana_primary_color]["500"],
+		secondary        = colors[vim.g.patana_secondary_color]["500"],
+		accent           = colors[vim.g.patana_accent_color]["500"],
 
     -- Special
 		cursor_line      = colors.grays["150"],
@@ -165,9 +107,9 @@ else
 		norm_very_subtle = colors.grays["200"],
 
 		-- Colors
-		primary          = colors[primary]["500"],
-		secondary        = colors[secondary]["500"],
-		accent           = colors[accent]["500"],
+		primary          = colors[vim.g.patana_primary_color]["500"],
+		secondary        = colors[vim.g.patana_secondary_color]["500"],
+		accent           = colors[vim.g.patana_accent_color]["500"],
 
 		-- Special
 		cursor_line      = colors.grays["800"],
@@ -272,8 +214,8 @@ local hlgroups = {
 	Search            = { bg = palette.search },
 	SignColumn        = { bg = palette.bg, fg = palette.norm, bold = true },
 	SpecialKey        = { fg = palette.norm_subtle },
-	StatusLine        = vim.tbl_extend("force", contrasting[primary], { bold = true }),
-	StatusLineNC      = contrasting[primary],
+	StatusLine        = vim.tbl_extend("force", contrasting[vim.g.patana_primary_color], { bold = true }),
+	StatusLineNC      = contrasting[vim.g.patana_primary_color],
 	StatusLineTerm    = { link = "StatusLine" },
 	StatusLineTermNC  = { link = "StatusLineNC" },
 	Substitute        = { link = "Search" },
