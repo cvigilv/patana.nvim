1dbc81289d77098ec5422f7a7cb6c43bcc782998
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-07-17 00:44:52 -0400

    refactor: extract color lookup to lua folder

--- colors/patana.lua
+++ colors/patana.lua
@@ -22,71 +22,14 @@ vim.g.patana_sidebar_filetypes = {
 	"gitcommit",
 }
 
-local colors = {
-	grays = {
-		["000"] = "#000000",
-		["050"] = "#0E0E0E",
-		["100"] = "#1C1C1C", -- darker
-		["150"] = "#2A2A29",
-		["200"] = "#393837",
-		["250"] = "#474645",
-		["300"] = "#555453", -- dark
-		["350"] = "#636261",
-		["400"] = "#71706F",
-		["450"] = "#807F7D",
-		["500"] = "#8E8D8A", -- normal
-		["550"] = "#9C9B98",
-		["600"] = "#AAA9A6",
-		["650"] = "#B8B7B4",
-		["700"] = "#C6C5C2", -- light
-		["750"] = "#D5D3D0",
-		["800"] = "#E3E1DD",
-		["850"] = "#F1EFEB",
-		["900"] = "#FFFDF9", -- lighter
-	},
-	greens = {
-		["100"] = "#182612", -- darker
-		["200"] = "#304d23",
-		["300"] = "#487335", -- dark
-		["400"] = "#609a47",
-		["500"] = "#7bb662", -- normal
-		["600"] = "#96c581",
-		["700"] = "#b0d4a1", -- light
-		["800"] = "#cae2c0",
-		["900"] = "#e5f1e0", -- lighter
-	},
-	oranges = {
-		["100"] = "#361903", -- darker
-		["200"] = "#6b3105",
-		["300"] = "#a14a08", -- dark
-		["400"] = "#d6630a",
-		["500"] = "#f58024", -- normal
-		["600"] = "#f79850",
-		["700"] = "#f9b27b", -- light
-		["800"] = "#fbcca7",
-		["900"] = "#fde5d3", -- lighter
-	},
-
-	purples = {
-		["100"] = "#1e1023", -- darker
-		["200"] = "#3d1f47",
-		["300"] = "#5b2f6a", -- dark
-		["400"] = "#7a3e8e",
-		["500"] = "#994fb2", -- normal
-		["600"] = "#ad71c1",
-		["700"] = "#c195d0", -- light
-		["800"] = "#d6b8e0",
-		["900"] = "#eadcef", -- lighter
-	},
+local colors = require("patana.colors")
+local contrasting = {
+	greens = { fg = colors.grays["000"], bg = colors.greens["700"] },
+	oranges = { fg = colors.grays["000"], bg = colors.oranges["700"] },
+	purples = { fg = colors.grays["900"], bg = colors.purples["700"] },
 }
 
--- NOTE: The aim is the following:
---       - Mainly monochrome, with color for literals (`literal`) and errors (`error`)
---       - Things that require my attention should be in *bold* typeface
---       - Things that don't require my attention should be in *italic* typeface
---       - UI stuff that require attention should be highlighted with accent color
---       - Visual and search will have a "highlighter" style background (`visual`)
---       - Diff status also colored, but it should be subtle
+-- stylua: ignore start
 local palette
 if vim.o.background == "dark" then
 	palette = {
