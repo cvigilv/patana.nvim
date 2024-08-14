fb8b3123c6143929ecb0c674fdccf7d88ad24a40
Carlos Vigil Vásquez <cvigil@proterabio.com>
2024-08-14 14:55:47 -0400

    feat: add high contrast colors to palette

--- lua/patana/palette.lua
+++ lua/patana/palette.lua
@@ -6,9 +6,9 @@ if vim.o.background == "dark" then
 	palette = {
     -- Normal
 		bg               = vim.g.patana_high_contrast and colors.grays["000"] or colors.grays["100"],
-		bg_subtle        = colors.grays["150"],
-		bg_very_subtle   = colors.grays["250"],
-		norm             = colors.grays["850"],
+		bg_subtle        = vim.g.patana_high_contrast and colors.grays["050"] or colors.grays["150"],
+		bg_very_subtle   = vim.g.patana_high_contrast and colors.grays["150"] or colors.grays["250"],
+		norm             = vim.g.patana_high_contrast and colors.grays["900"] or colors.grays["850"],
 		norm_subtle      = colors.grays["800"],
 		norm_very_subtle = colors.grays["700"],
 
@@ -18,19 +18,19 @@ if vim.o.background == "dark" then
 		accent           = colors[vim.g.patana_accent_color]["600"],
 
     -- Special
-		cursor_line      = colors.grays["150"],
+		cursor_line      = vim.g.patana_high_contrast and colors.grays["050"] or colors.grays["150"],
 		comment          = colors.grays["450"],
 		oob              = colors.grays["000"],
 		search           = colors[vim.g.patana_primary_color]["200"],
     contrasting      = colors[vim.g.patana_primary_color]["700"],
 	}
-else
+else -- bg=light
 	palette = {
 		-- Normal
 		bg               = vim.g.patana_high_contrast and colors.grays["900"] or colors.grays["850"],
 		bg_subtle        = colors.grays["800"],
 		bg_very_subtle   = colors.grays["700"],
-		norm             = colors.grays["050"],
+		norm             = vim.g.patana_high_contrast and colors.grays["000"] or colors.grays["050"],
 		norm_subtle      = colors.grays["100"],
 		norm_very_subtle = colors.grays["200"],
 
@@ -40,7 +40,7 @@ else
 		accent           = colors[vim.g.patana_accent_color]["400"],
 
 		-- Special
-		cursor_line      = colors.grays["800"],
+		cursor_line      = vim.g.patana_high_contrast and colors.grays["850"] or  colors.grays["800"],
 		comment          = colors.grays["450"],
 		oob              = colors.grays["900"],
 		search           = colors[vim.g.patana_primary_color]["800"],
